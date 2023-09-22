63d6841b527abf1e93948cac4f3b6f9acc34c51d
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-09-23 01:22:53 +0600

    math added

---
+++ math.js
@@ -0,0 +1,7 @@
+const num = Math.pow(4,2);
+console.log(num);
+
+const num1 = 15;
+const num2 = 35;
+let gap = num1 - num2;
+console.log(Math.abs(gap));
--- slice.js
+++ slice.js
@@ -2,3 +2,11 @@ const lines = 'sada sada kala kala, hani tumi kala kala';
 const splitLine = lines.split('k')
 const sliceLine = lines.slice(5,10)
 console.log(sliceLine); 
+const index = 5;
+const index3 = lines.charAt(index);
+console.log(index3);
+
+let trimTest = '     Hello World    ';
+console.log(trimTest.trimEnd());
+const numberItem = [12,23,34,45,56];
+console.log(numberItem.join('-'));
